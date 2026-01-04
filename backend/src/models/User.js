82f67be62a1ab00fe4fd2e45ee764e2349526ff9
Rankela Venkata Arjun <venkataarjun.r@gmail.com>
2026-01-04 14:53:19 +0530

    Fixed a bug in user.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -39,16 +39,10 @@ const userSchema = new mongoose.Schema(
 );
 
 /* -------- PASSWORD HASHING -------- */
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 /* -------- PASSWORD COMPARISON -------- */
