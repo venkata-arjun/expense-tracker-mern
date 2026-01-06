068cb23b2be6af4a59c8270fd7aaac31308877ab
Rankela Venkata Arjun <venkataarjun.r@gmail.com>
2026-01-06 09:15:16 +0530

    Deploy issues resolved

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -23,13 +23,12 @@ export const register = async (req, res) => {
   });
   await seedCategoriesForUser(user._id);
 
-
   const token = generateToken(user._id);
 
   res
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
@@ -66,7 +65,7 @@ export const login = async (req, res) => {
   res
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "https://finlyt-web.vercel.app"],
     credentials: true,
   })
 );
